50dcb57a2bbe120a2202eac948e5cbf4e59e25cb
PratyakshG <pincodeazm@gmail.com>
2025-06-15 21:01:03 +0530

    Implement Clerk Auth and Supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
   return (
-    <div className='text-2xl font-bold'>
-      Home Page
-    </div>
-  )
-}
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
 
-export default Page
+      <section className="home-section">
+        <CompanionCard
+          id="1"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="Science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="2"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="3"
+          name="Verba the Vocab Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
